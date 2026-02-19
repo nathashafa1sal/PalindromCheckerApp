95ef1e1269c70c55e1fc47408526d806231e2b9c
nathashafa1sal <nf6441@srmist.edu.in>
2026-02-19 11:19:51 +0530

    [Nathasha]Added[Usecase3]

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -1,26 +1,32 @@
-// version 2.0
+// version 3.0
 //author Nathasha
-//Usecase2: Hardcorepalindrome
+//Usecase3: ReverseString
+import java.util.Scanner;
+
 public class PalindromeChecker {
 
     public static void main(String[] args) {
 
-        // Hardcoded string
-        String word = "madam";
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter a string: ");
+        String original = scanner.nextLine();
 
         String reversed = "";
 
-
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        // Reverse string using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
-        // Check if palindrome using if-else
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome.");
+        // Compare original and reversed string
+        if (original.equals(reversed)) {
+            System.out.println("The given string is a Palindrome.");
         } else {
-            System.out.println(word + " is NOT a Palindrome.");
+            System.out.println("The given string is NOT a Palindrome.");
         }
+
+        scanner.close();
     }
 }
+
