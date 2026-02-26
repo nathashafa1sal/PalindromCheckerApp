fe7b42e58bbcc23d943b1e30acc956b9ddfd268f
nathashafa1sal <nf6441@srmist.edu.in>
2026-02-26 10:36:30 +0530

    [Nathasha]Added[Objectoriented]

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,45 +1,28 @@
-//version 10.0
+//version 11.0
 //author Nathasha
-//useCase 10:Casesensitive
-import java.util.Scanner;
+//useCase 11:Objectoriented
 
 public class PalindromeCheckerApp {
 
-    public static boolean isPalindrome(String str) {
+    // Public method exposed to outside
+    public boolean checkPalindrome(String input) {
+
+        if (input == null)
+            return false;
+
+        String normalized = input.toLowerCase().replaceAll("\\s+", "");
 
         int start = 0;
-        int end = str.length() - 1;
+        int end = normalized.length() - 1;
 
         while (start < end) {
-            if (str.charAt(start) != str.charAt(end)) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
                 return false;
             }
             start++;
             end--;
         }
+
         return true;
-    }
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String input = sc.nextLine();
-
-        // Normalize string
-        String normalized = input
-                .toLowerCase()
-                .replaceAll("\\s+", "");  // remove spaces
-
-        // Optional advanced cleaning:
-        // .replaceAll("[^a-z0-9]", "");
-
-        if (isPalindrome(normalized)) {
-            System.out.println("The string is a Palindrome (ignoring case & spaces).");
-        } else {
-            System.out.println("The string is NOT a Palindrome.");
-        }
-
-        sc.close();
     }
 }
